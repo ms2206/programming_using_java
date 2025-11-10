2aeb01836ec21d67294323d78c30de02377c3eb7
Matthew Spriggs <matspriggs@gmail.com>
2025-11-10 16:39:34 +0000

    Add degrees class with Celsius to Fahrenheit conversion method

---
+++ Mon/degrees.java
@@ -0,0 +1,7 @@
+public class degrees {
+
+    public static double convertToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+}
---
+++ demos/hello.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class hello {
+
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+        JOptionPane.showMessageDialog(null,
+                "Hello, World!",
+                "Greeting",
+                JOptionPane.WARNING_MESSAGE);
+    }
+}
