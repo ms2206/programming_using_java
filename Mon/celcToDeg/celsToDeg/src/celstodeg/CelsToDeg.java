55bbae234af815ba28a466f02ea9a462b71b947c
Matthew Spriggs <matspriggs@gmail.com>
2025-11-11 16:24:38 +0000

    Refactor CelsToDeg main method to c2d and enhance ForLoopDegF to display Celsius to Fahrenheit conversion

--- Mon/celcToDeg/celsToDeg/src/celstodeg/CelsToDeg.java
+++ Mon/celcToDeg/celsToDeg/src/celstodeg/CelsToDeg.java
@@ -15,7 +15,7 @@ public class CelsToDeg {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void c2d(String[] args) {
         // TODO code application logic here
 
         // Get celsius
--- tue/forLoopDegF/forLoopDegF/src/forloopdegf/ForLoopDegF.java
+++ tue/forLoopDegF/forLoopDegF/src/forloopdegf/ForLoopDegF.java
@@ -4,9 +4,6 @@
  */
 package forloopdegf;
 
-
-
-
 /**
  *
  * @author mspriggs
@@ -19,7 +16,22 @@ public class ForLoopDegF {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        
+        System.out.println("Cels. Temp." + "    " + "Fahr. Temp."); 
+        System.out.println("-------------------------");
+
+        for (float i = 40; i > 30; i--) {
+             
+
+            double degC = convertFtoC(i);
+            System.out.println(i + "                " + degC);
+
+        }
+
     }
-    
+
+    public static double convertFtoC(double degF) {
+        double degC = (degF * 9 / 5) + 32;
+        return degC;
+    }
+
 }
